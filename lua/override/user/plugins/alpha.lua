39c0ed0ad2e27e3569a0d573d97e78bfd3551911
Lucas Nunzi <lucas.nunzi@life5.com>
2025-05-16 10:58:52 +0200

    Initial commit.

---
+++ lua/override/user/plugins/alpha.lua
@@ -0,0 +1,54 @@
+return { "goolord/alpha-nvim", enabled = false }
+-- local alpha = require("alpha")
+-- local startify = require("alpha.themes.startify")
+--
+-- startify.section.header.val = {
+-- 	"                                         ",
+-- 	"                                         ",
+-- 	"                                         ",
+-- 	"██╗░░░░░██╗░░░██╗░█████╗░░█████╗░░██████╗",
+-- 	"██║░░░░░██║░░░██║██╔══██╗██╔══██╗██╔════╝",
+-- 	"██║░░░░░██║░░░██║██║░░╚═╝███████║╚█████╗░",
+-- 	"██║░░░░░██║░░░██║██║░░██╗██╔══██║░╚═══██╗",
+-- 	"███████╗╚██████╔╝╚█████╔╝██║░░██║██████╔╝",
+-- 	"╚══════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═════╝░",
+-- 	"                                         ",
+-- 	"                                         ",
+-- 	"                                         ",
+-- }
+-- startify.config.opts.noautocmd = true
+-- -- startify.config.opts.position = "right"
+-- startify.config.opts.margin = 100
+--
+-- startify.section.top_buttons.val = {
+-- 	startify.button("e", "  New file", "<cmd>ene <CR>"),
+--     { type = "padding", val = 2 },
+-- 	{ type = "text", val = "Search ", opts = { hl = "SpecialComment", position = "center" } },
+-- 	{ type = "text", val = "-------------------------------------------------------------", opts = { hl = "SpecialComment", position = "center" } },
+-- 	startify.button("SPC f f", "󰈞  Find file"),
+-- 	startify.button("SPC f o", "󰊄  Recently opened files"),
+-- 	startify.button("SPC f g", "󰈬  Find word"),
+-- }
+-- startify.section.bottom_buttons.val = {
+--     { type = "padding", val = 4 },
+-- 	{ type = "text", val = "Sessions", opts = { hl = "SpecialComment", position = "center" } },
+-- 	{ type = "text", val = "-------------------------------------------------------------", opts = { hl = "SpecialComment", position = "center" } },
+-- 	startify.button("SPC s l", "  Open lasts sessions"),
+--     { type = "padding", val = 4 },
+-- 	{ type = "text", val = "-------------------------------------------------------------", opts = { hl = "SpecialComment", position = "center" } },
+-- 	startify.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+-- 	startify.button(":help", "if you are new!", ":help nvim<CR>"),
+-- 	startify.button(":checkhealth", "to optimize Nvim", ":checkhealth<CR>"),
+--   startify.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
+--
+--
+--
+--
+--
+-- }
+-- startify.section.mru.val = {}
+-- startify.section.mru_cwd.val = {}
+--
+-- -- vim.cmd([[autocmd User AlphaReady echo 'Happy coding!!!']])
+--
+-- alpha.setup(startify.config)
