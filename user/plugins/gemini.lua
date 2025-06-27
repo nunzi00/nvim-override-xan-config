ccdb86bcebbd24cfa8c5540994d61db82e631b2c
Lucas Nunzi <lucas.nunzi@life5.com>
2025-06-27 08:56:40 +0000

    gemini mapping

--- user/plugins/gemini.lua
+++ user/plugins/gemini.lua
@@ -1,4 +1,13 @@
 return {
   'kiddos/gemini.nvim',
-  opts = {}
+  opts = {
+    hints = {
+      insert_result_key = '<C-a>',
+    },
+    completion = {
+      completion_delay = 400,
+      move_cursor_end = true,
+      insert_result_key = '<C-a>',
+    },
+  }
 }
