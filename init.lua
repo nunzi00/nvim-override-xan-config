f8d285d98c749927f1603bbfabef57105e5efd6b
Lucas <nunzi00@gmail.com>
2025-07-10 09:49:51 +0200

    force theme

--- init.lua
+++ init.lua
@@ -2,3 +2,8 @@ require 'override.user.keymap'
 require 'override.user.autocommands'
 require 'override.user.filetypes'
 vim.opt.mouse='a'
+onenord_load = function()
+  vim.cmd [[colorscheme onenord]]
+end
+
+return onenord_load()
